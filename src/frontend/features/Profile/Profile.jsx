b0ee4d3c3c01fe52a59e87b999ac9310b67b0200
Om Kakatkar <omkakatkar@gmail.com>
2022-07-20 14:30:24 +0530

    [FIX] App crash on Profile route

--- src/frontend/features/Profile/Profile.jsx
+++ src/frontend/features/Profile/Profile.jsx
@@ -1,9 +1,11 @@
-import { useSelector } from "react-redux";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
 import ModalCard from "../../components/ModalCard/ModalCard";
 import ProfileUpdate from "../../components/ProfileUpdate/ProfileUpdate";
 import useDetectClickOutside from "../../hooks/useDetectClickOutside";
 import "./Profile.css";
+import { handleGetAllUsers } from "./userSlice";
 
 function Profile() {
 	const { userId } = useParams();
@@ -11,53 +13,64 @@ function Profile() {
 	const { user: currentUser } = useSelector((store) => store.auth);
 	const { triggerRef, nodeRef, showItem, setShowItem } =
 		useDetectClickOutside();
+	const dispatch = useDispatch();
 
 	const user = userId
 		? allUsers.filter(({ _id }) => _id === userId)[0]
 		: currentUser;
 
+	useEffect(() => {
+		if (!allUsers.length) {
+			dispatch(handleGetAllUsers());
+		}
+	}, [allUsers.length, dispatch]);
+
 	return (
-		<div className="text-white">
-			{!userId && (
-				<button className="profile-edit" ref={triggerRef}>
-					Edit
-				</button>
-			)}
-			<div className="image circular">
-				<img src={user.image} alt="Profile" />
-			</div>
-			<div>
-				<div className="flex flex-column text-center">
-					<span className="text-xhuge">
-						{user?.firstName} {user?.lastName}
-					</span>
+		<>
+			{user && (
+				<div className="text-white">
+					{!userId && (
+						<button className="profile-edit" ref={triggerRef}>
+							Edit
+						</button>
+					)}
+					<div className="image circular">
+						<img src={user.image} alt="Profile" />
+					</div>
+					<div>
+						<div className="flex flex-column text-center">
+							<span className="text-xhuge">
+								{user?.firstName} {user?.lastName}
+							</span>
+						</div>
+						<div className="flex flex-column text-center">
+							<span className="text-xlg">{user?.followers?.length}</span>
+							<span className="text-xlg">Followers</span>
+						</div>
+					</div>
+					<div className="text-center">
+						<span>Site :</span>
+						<a
+							href={user.site}
+							rel="noreferrer"
+							target="_blank"
+							className="text-green"
+						>
+							{user.site}
+						</a>
+					</div>
+					<div className="text-center">
+						<span>Bio :</span>
+						<span>{user.bio}</span>
+					</div>
+					{showItem && (
+						<ModalCard ref={nodeRef}>
+							<ProfileUpdate setShowItem={setShowItem} />
+						</ModalCard>
+					)}
 				</div>
-				<div className="flex flex-column text-center">
-					<span className="text-xlg">{user?.followers?.length}</span>
-					<span className="text-xlg">Followers</span>
-				</div>
-			</div>
-			<div className="text-center">
-				<span>Site :</span>
-				<a
-					href={user.site}
-					rel="noreferrer"
-					target="_blank"
-					className="text-green"
-				>
-					{user.site}
-				</a>
-			</div>
-			<div className="text-center">
-				<span>Bio :</span>
-				<span>{user.bio}</span>
-			</div>
-			{showItem && (
-				<ModalCard ref={nodeRef}>
-					<ProfileUpdate setShowItem={setShowItem} />
-				</ModalCard>
 			)}
-		</div>
+		</>
 	);
 }
 export default Profile;
